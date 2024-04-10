7dac4a154f4ba5074e0da604514c09717776f2b9
Михаил Кориков <oneepicfailpro@gmail.com>
2024-04-10 13:19:51 +1000

    Финал

--- Проверка/2.1.cpp
+++ Проверка/2.1.cpp
@@ -4,7 +4,6 @@
 #include <fstream> // для чтения и записи файла
 #include <string> // для работы со строками
 #include <iomanip> // для перевода времени из(fixed);
-#include <algorithm> // для max min
 using namespace std;
 using namespace chrono;
 
@@ -16,18 +15,12 @@ struct key {
     }group;
 
     struct fio { //ФИО
-        string surname; // фамилия
-        string name; // имя
-        string patronymic; // отчество
+        string fio;
     }fio;
 
     struct Direction {
         string title; // название направления
     }dir;
-
-    const string to_String_Fio() { // для возвращения целой строки FIO
-        return fio.surname + " " + fio.name + " " + fio.patronymic;
-    }
 };
 
 
@@ -105,7 +98,7 @@ void bad_symbol(const string& pattern,int& pattern_len,int bad_symbols[]) {
     }
 }
 
-void good_suffix(const string& pattern, int& pattern_len, int good_suffixs[]) {
+void good_suffix(const string& pattern,int& pattern_len, int good_suffixs[]) {
     int f[pattern_max_len];
     for (int i = 0; i < pattern_len; ++i) {
         good_suffixs[i] = 0;
@@ -140,48 +133,51 @@ int max_for_TBM(int a, int b, int c) {
 
 bool TBM(string const str,int str_len,string const pattern,int pattern_len,int pattern_count,int good_suffixs[pattern_max_len],int bad_symbols[alphabet_size]) {
     int count = 0; // счётчик вхождения паттерна
-    int i = 0, u = 0, shift = 0;
-    while (i <= str_len - pattern_len) {
-        int j = pattern_len - 1;
-        while ((j >= 0) && (pattern[j] == str[i + j])) {
-            --j;
-            if ((u != 0) && (j == pattern_len - 1 - shift)) {
-                j -= u;
+    int str_index = 0; // текущий индекс в тексте
+    int good_suffix_rem = 0; // Остаток от сдвига по "Хорошему суффиксу"
+    int pattern_shift = pattern_len; // сдвиг шаблона
+    while (str_index <= str_len - pattern_len) { // идём по строке
+        int pattern_index = pattern_len - 1; // текущий индекс в шаблоне
+
+        while ((pattern_index >= 0) && (pattern[pattern_index] == str[str_index + pattern_index])) { // Проверка совпадения символов шаблона и текста
+            --pattern_index; // сдвигаемся по шаблону
+            if ((good_suffix_rem != 0) && (pattern_index == pattern_len - 1 - pattern_shift)) {// проверка условия хорошего суффикса
+                pattern_index -= good_suffix_rem;
             }
         }
-        if (j < 0) {
-            count++;
+        if (pattern_index < 0) {// нашлось вхождение
+            count++;// добавляем
             if (count == pattern_count) {
-                return true;
+                return true; // если кол-во равно, то выходим
             }
-            shift = good_suffixs[0];
-            u = pattern_len - shift;
+            pattern_shift = good_suffixs[0];
+            good_suffix_rem = pattern_len - pattern_shift;
         }
-        else {
-            int v = pattern_len - 1 - j;
-            int turbo_Shift = u - v;
-            int bad_symbol_shift = bad_symbols[str[i + j] - pattern_len + j + 1];
-            shift = max_for_TBM(turbo_Shift,bad_symbol_shift,good_suffixs[j + 1]);
-            if (shift == good_suffixs[j + 1]) {
-                if ((pattern_len - shift) < v) {
-                    u = pattern_len - shift;
+        else { // если не совпал последний символ
+            int v = pattern_len - 1 - pattern_index;
+            int turbo_Shift = good_suffix_rem - v;
+            int bad_symbol_shift = bad_symbols[str[str_index + pattern_index] - pattern_len + pattern_index + 1];
+            pattern_shift = max_for_TBM(turbo_Shift,bad_symbol_shift,good_suffixs[pattern_index + 1]);
+            if (pattern_shift == good_suffixs[pattern_index + 1]) {
+                if ((pattern_len - pattern_shift) < v) {
+                    good_suffix_rem = pattern_len - pattern_shift;
                 }
                 else {
-                    u = v;
+                    good_suffix_rem = v;
                 }
                 //u = min(pattern_len - shift, v);
             }
             else {
                 if (turbo_Shift < bad_symbol_shift) {
-                    if (shift < (u + 1)) {
-                        shift = u + 1;
+                    if (pattern_shift < (good_suffix_rem + 1)) {
+                        pattern_shift = good_suffix_rem + 1;
                     }
                     //shift = max(shift,u+1);
                 }
-                u = 0;
+                good_suffix_rem = 0;
             }
         }
-        i += shift;
+        str_index += pattern_shift; // обновляем текущий индекс в тексте
     }
     return false;
 };
@@ -191,7 +187,7 @@ bool TBM(string const str,int str_len,string const pattern,int pattern_len,int p
 void read_file(vector<key>& arr, const string& filename) {
     string line;
     int count = 0;
-    int p1, p2, p3, p4 = 0;
+    int p1, p2 = 0;
 
     ifstream in(filename);
     if (in.is_open()) {
@@ -200,14 +196,10 @@ void read_file(vector<key>& arr, const string& filename) {
             key k;
             k.group.l = line[0];
             k.group.num = stoi(line.substr(1, 4));
-            p1 = line.find_first_of(' ');
-            p2 = line.find_first_of(' ', p1 + 1);
-            k.fio.surname = line.substr(p1 + 1, p2 - p1 - 1);
-            p3 = line.find_first_of(' ', p2 + 1);
-            k.fio.name = line.substr(p2 + 1, p3 - p2 - 1);
-            p4 = line.find_last_of(' ');
-            k.fio.patronymic = line.substr(p3 + 1, p4 - p3 - 1);
-            k.dir.title = line.substr(p4 + 1);
+            p1 = line.find(' ');
+            p2 = line.rfind(' ');
+            k.fio.fio = line.substr(p1, p2-p1);
+            k.dir.title = line.substr(p2 + 1);
             k.s = count;
             arr.push_back(k);
         }
@@ -222,7 +214,7 @@ void write_file(vector<key>& arr, duration<double> diff, const string& filename)
     long n = arr.size();
     if (file.is_open()) {
         for (int i = 0; i < n; i++) {
-            file << arr[i].group.l << arr[i].group.num << " " << arr[i].fio.surname << " " << arr[i].fio.name << " " << arr[i].fio.patronymic << " " << arr[i].dir.title << " " << arr[i].s << "\n";
+            file << arr[i].group.l << arr[i].group.num << " " << arr[i].fio.fio << " " << arr[i].dir.title << " " << arr[i].s << "\n";
         }
         file << fixed << diff.count() << " s" << "\n";
     }
@@ -245,12 +237,12 @@ void Call_Rabin_Karp() {
     string const pattern_title = "H"; // паттерн направления
     int pattern_title_len = pattern_title.length(); // длина паттерна направления
     int pattern_title_hash = first_hash(pattern_title, pattern_title_len);
-    int pattern_title_count = 2; // количество вхождений паттерна в строке направления
+    int pattern_title_count = 1; // количество вхождений паттерна в строке направления
 
     auto startRK = high_resolution_clock::now(); // время начала 
     for (int i = 0; i < n; i++) {
 
-        string str_fio = arrRK[i].to_String_Fio();// строка поля фио
+        string str_fio = arrRK[i].fio.fio;// строка поля фио
         int str_fio_len = str_fio.length(); // длина строки фио
         int str_fio_hash = first_hash(str_fio, pattern_fio_len); // хэш фио
 
@@ -259,7 +251,7 @@ void Call_Rabin_Karp() {
         int str_title_hash = first_hash(str_title, pattern_title_len); // хэш направления
 
         if (RK(str_fio, str_fio_len, str_fio_hash, pattern_fio, pattern_fio_len, pattern_fio_hash, pattern_fio_count) && RK(str_title,str_title_len,str_title_hash,pattern_title,pattern_title_len,pattern_title_hash,pattern_title_count)) {
-            answer.push_back(arrRK[i]);
+            answer.push_back(arrRK[i]);//закидываем ответ
         }
        
     }
@@ -285,22 +277,22 @@ void Call_Turbo_Boyer_Moore() {
 
     string const pattern_title = "H"; // паттерн направления
     int pattern_title_len = pattern_title.length(); // длина паттерна направления
-    int pattern_title_count = 2; // количество вхождений паттерна в строке направления
-    int good_title_suffix[pattern_max_len], bad_title_symbols[alphabet_size];
-    good_suffix(pattern_title, pattern_title_len, good_title_suffix);
-    bad_symbol(pattern_title, pattern_title_len, bad_title_symbols);
+    int pattern_title_count = 1; // количество вхождений паттерна в строке направления
+    int good_title_suffix[pattern_max_len], bad_title_symbols[alphabet_size]; // объявление массивов "хорошего суффикса" и "плохого символа"
+    good_suffix(pattern_title, pattern_title_len, good_title_suffix); // заполнение
+    bad_symbol(pattern_title, pattern_title_len, bad_title_symbols); // заполнение
 
     auto startTBM = high_resolution_clock::now(); // время начала
     for (int i = 0; i < n; i++) {
 
-        string str_fio = arrTBM[i].to_String_Fio();// строка поля фио
+        string str_fio = arrTBM[i].fio.fio;// строка поля фио
         int str_fio_len = str_fio.length(); // длина строки фио
 
         string str_title = arrTBM[i].dir.title; // строка поля направления
         int str_title_len = str_title.length(); // длина строки направления
 
         if ((TBM(str_fio, str_fio_len, pattern_fio, pattern_fio_len, pattern_fio_count,good_fio_suffixs,bad_fio_symbols)) && (TBM(str_title,str_title_len,pattern_title,pattern_title_len,pattern_title_count,good_title_suffix,bad_title_symbols))) {
-            answer.push_back(arrTBM[i]);
+            answer.push_back(arrTBM[i]); // закидываем ответ
         }
     }
     auto endTBM = high_resolution_clock::now(); // время конца
